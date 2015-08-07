fc7433dc005b3ed9eba488daa60b09674078549e
dengxianglong <1101607349@qq.com>
2015-08-07 16:58:45 +0800

    19.02

--- Duilib3/Notify.cpp
+++ Duilib3/Notify.cpp
@@ -39,14 +39,14 @@ public:
             CControlUI *pWnd = new CButtonUI;
             pWnd->SetName(_T("btnHello"));      // 设置控件的名称，这个名称用于标识每一个控件，必须唯一，相当于MFC里面的控件ID
             pWnd->SetText(_T("Hello World"));   // 设置文字
-            pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
-
             m_PaintManager.Init(m_hWnd);
             m_PaintManager.AttachDialog(pWnd);
+            pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+
+
             m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
             return lRes;
         }
-
         if( m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes) ) 
         {
             return lRes;
--- hello word/hello word.cpp
+++ hello word/hello word.cpp
@@ -3,6 +3,11 @@
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
-    ::MessageBox(NULL, _T("Hello World !"), NULL, NULL);
-    return 0;
+    int result = MessageBox( NULL , TEXT("这是对话框") , TEXT("你好") , MB_ICONINFORMATION|MB_YESNO);
+    switch(result)/*注意！使用Unicode应用TEXT包围字串*/
+{
+    case IDYES:MessageBox(NULL,TEXT("您选择了YES"),TEXT("YES"),MB_OK);break;
+    case IDNO:MessageBox(NULL,TEXT("您选择了NO"),TEXT("NO"),MB_OK);break;
+}
+return 0;
 }
